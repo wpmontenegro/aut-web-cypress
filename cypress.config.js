30113084fd4f7773db5360cb70836b639135aeab
William Montenegro <williammontenegro4d@gmail.com>
2025-03-29 14:36:44 -0500

    first test and integration

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,21 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    specPattern: "cypress/e2e/**/*.feature",
+    async setupNodeEvents(on, config) {
+      await preprocessor.addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
     },
   },
 });
