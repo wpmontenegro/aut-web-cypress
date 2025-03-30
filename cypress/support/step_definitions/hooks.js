1e1317fed4e8d30a4ed6da14a096cec981a9a447
William Montenegro <williammontenegro4d@gmail.com>
2025-03-29 23:17:31 -0500

    log cypress

--- cypress/support/step_definitions/hooks.js
+++ cypress/support/step_definitions/hooks.js
@@ -5,15 +5,13 @@ import {
 } from "@badeball/cypress-cucumber-preprocessor";
 
 BeforeAll(() => {
-  console.log("This hook will be executed once at the beginnig of a feature.");
+  cy.log("This hook will be executed once at the beginnig of a feature.");
 });
 
 Before({ tags: "@Login" }, () => {
-  console.log(
-    "This hook will be executed before scenarios tagged with @Login."
-  );
+  cy.log("This hook will be executed before scenarios tagged with @Login.");
 });
 
 After(() => {
-  console.log("This hook will be executed after all scenarios.");
+  cy.log("This hook will be executed after all scenarios.");
 });
